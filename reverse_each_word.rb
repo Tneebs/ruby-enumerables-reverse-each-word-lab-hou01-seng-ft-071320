bb826c819ad6c5b8e140648b95cbf0318d93539d
Ted Neben <tedneben@gmail.com>
2020-06-12 20:10:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,15 @@ def reverse_each_word(sentence)
   equals = reverse.join(" ")
   equals
 end
+
+def reverse_each_word(sentence)
+  split_sentence = sentence.split
+  reverse = []
+  
+  split_sentence.collect do |arr|
+    rev_word = arr.reverse 
+    reverse << rev_word 
+  end
+  equals = reverse.join(" ")
+  equals
+end
