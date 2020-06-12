8ee928289c45ea3779d8526aad919719db5869b6
Ted Neben <tedneben@gmail.com>
2020-06-12 19:57:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,2 @@
 def reverse_each_word(string)
-  split_string = string.split(" ")
-  reverse = []
-  split_string.each do |x|
-    string.size.times { reverse << split_string.pop }
-  end
-  reverse
-end
+  
